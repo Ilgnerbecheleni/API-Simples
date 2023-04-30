04812eb8bb9f6eca55ffa97b656f4c95eb953b0d
Ilgner Aparecido Becheleni <81272773+Ilgnerbecheleni@users.noreply.github.com>
2023-04-29 22:42:33 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'segredo';
 global.EMAIL_TMPL = '<strong>{0}</strong>';
 
 module.exports = {
-    connectionString:'mongodb+srv://ilgner:ilgner@ilgnerbd.5vod6ae.mongodb.net/acessControl?retryWrites=true&w=majority'
+    connectionString:'sua_string'
 
 
 }
